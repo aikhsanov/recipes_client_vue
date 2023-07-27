d27f1da45e7e68e9530e4b9d5a96419310900b73
ai-strings <adrstrings@gmail.com>
2023-07-28 00:05:19 +0300

    +add Register component for test purposes

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,14 +3,15 @@ import type { AxiosRequestConfig } from 'axios';
 import router from '@/router';
 
 const options = {
-  baseURL: 'http://api.receipts.haemmid.ru/',
+  // baseURL: 'http://api.receipts.haemmid.ru/',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     Accept: 'application/json',
   },
 } as CreateAxiosDefaults;
 
-const api = axios.create(options);
-api.interceptors.response.use(
+const AxiosInstance = axios.create(options);
+AxiosInstance.interceptors.response.use(
   (resp) => {
     return resp;
   },
@@ -23,4 +24,12 @@ api.interceptors.response.use(
     }
   }
 );
-export default api;
+
+const api = (axios) => ({
+  get: (url, config, body) => axios.get(url, config, body),
+  post: (url, body, config) => axios.post(url, body, config),
+  patch: (url, body, config) => axios.patch(url, body, config),
+  delete: (url, config) => axios.delete(url, config),
+});
+
+export default api(AxiosInstance);
---
+++ src/views/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <input v-model="username" id="username" placeholder="username" />
+  <input v-model="email" id="email" placeholder="email" />
+  <input v-model="password" id="password" placeholder="password" />
+  <button type="submit" @click.prevent="register">Регистрация</button>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+};
+</script>
+<script setup>
+import { ref } from 'vue';
+import api from '../config/api';
+
+const email = ref('');
+const username = ref('');
+const password = ref('');
+
+async function register() {
+  const res = await api.post('/auth/register', {
+    email: email.value,
+    username: username.value,
+    password: password.value,
+  });
+  console.log(res);
+}
+</script>
+
+<style scoped></style>
