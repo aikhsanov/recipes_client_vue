c4abec2b2b1e67b1d150b854dd8a6a5502f46114
ai-strings <adrstrings@gmail.com>
2023-08-28 22:49:40 +0300

    +add transition to modal

--- src/components/base/BaseModal.vue
+++ src/components/base/BaseModal.vue
@@ -1,24 +1,32 @@
 <template>
-  <div
-    v-if="isOpen"
-    @click="toggleOpen"
-    id="backdrop"
-    class="
-      w-full
-      h-full
-      bg-gray-900 bg-opacity-50
-      dark:bg-opacity-80
-      top-0
-      left-0
-      bottom-0
-      right-0
-      fixed
-    "
-  />
-  <div
-    v-if="isOpen"
-    id="modal-wrapper"
-    :class="`
+  <TransitionGroup
+    enter-active-class="duration-500 ease-out"
+    enter-from-class="transform opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="duration-300 ease-in"
+    leave-from-class="opacity-100"
+    leave-to-class="transform opacity-0"
+  >
+    <div
+      v-if="isOpen"
+      @click="toggleOpen"
+      id="backdrop"
+      class="
+        w-full
+        h-full
+        bg-gray-900 bg-opacity-50
+        dark:bg-opacity-80
+        top-0
+        left-0
+        bottom-0
+        right-0
+        fixed
+      "
+    />
+    <div
+      v-if="isOpen"
+      id="modal-wrapper"
+      :class="`
       overflow-x-hidden overflow-y-auto
       flex
       flex-row
@@ -32,52 +40,53 @@
       max-w-5xl
       z-70
       md:h-auto ${props.class || ''}`"
-  >
-    <slot name="prepend"> </slot>
-    <div class="flex flex-col w-full">
-      <div class="flex justify-between p-2" id="modal-close">
-        <slot name="modal-header-controls"></slot>
-        <button
-          type="button"
-          @click="toggleOpen"
-          class="
-            text-gray-400
-            bg-transparent
-            hover:bg-gray-200 hover:text-gray-900
-            rounded-lg
-            text-sm
-            p-1.5
-            ml-auto
-            inline-flex
-            items-center
-            dark:hover:bg-gray-800 dark:hover:text-white
-          "
-          data-modal-toggle="authentication-modal"
-        >
-          <svg
-            class="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    >
+      <slot name="prepend"></slot>
+      <div class="flex flex-col w-full">
+        <div class="flex justify-between p-2" id="modal-close">
+          <slot name="modal-header-controls"></slot>
+          <button
+            type="button"
+            @click="toggleOpen"
+            class="
+              text-gray-400
+              bg-transparent
+              hover:bg-gray-200 hover:text-gray-900
+              rounded-lg
+              text-sm
+              p-1.5
+              ml-auto
+              inline-flex
+              items-center
+              dark:hover:bg-gray-800 dark:hover:text-white
+            "
+            data-modal-toggle="authentication-modal"
           >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8 h-full relative">
-        <slot :toggle="toggleOpen">
-          <h4>Котнтент недоступен или отсутствует</h4>
-        </slot>
-      </div>
-      <div class="" id="modal-controls">
-        <slot name="controls"></slot>
+            <svg
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8 h-full relative">
+          <slot :toggle="toggleOpen">
+            <h4>Контент недоступен или отсутствует</h4>
+          </slot>
+        </div>
+        <div class="" id="modal-controls">
+          <slot name="controls"></slot>
+        </div>
       </div>
     </div>
-  </div>
+  </TransitionGroup>
 
   <slot name="activator" :toggle="toggleOpen"></slot>
 </template>
