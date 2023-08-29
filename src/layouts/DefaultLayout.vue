1cc34031dcad0efb01fd493a2657c1566be3f96e
aikhsanov <adrstrings@gmail.com>
2023-08-29 12:09:00 +0300

    !fix moved nav from mainPage to DefaultLayout.vue

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="container mx-auto min-h-screen p-4">
-    <!--    <header></header>-->
+    <header>
+      <nav class="h-16 flex flex-row items-start justify-between">
+        <Select placeholder="Поиск рецептов" class="w-64" searchable></Select>
+        <AuthForm v-if="isSignedIn" />
+        <div class="flex flex-row" v-else>
+          <ProfileMenu />
+          <BaseButton
+            class="mt-0 w-auto text-white font-bold bg-tomato-800 hover:bg-tomato-900 px-5 ml-5 py-2"
+            type="link"
+            to="/recipes/add"
+            text="Добавить рецепт"
+          />
+        </div>
+      </nav>
+    </header>
     <slot />
     <footer></footer>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-};
+<script setup lang="ts">
+import Select from '@/components/base/Select.vue';
+import ProfileMenu from '@/components/profile/ProfileMenu.vue';
+import AuthForm from '@/components/forms/AuthForm.vue';
+import BaseButton from '@/components/base/BaseButton.vue';
+import { useAuthStore } from '@/stores/auth';
+import { computed } from 'vue';
+
+const auth = useAuthStore();
+
+const isSignedIn = computed<boolean>(() => Object.values(auth.getMe).length > 0);
 </script>
 
 <style scoped></style>
--- src/views/main/MainPage.vue
+++ src/views/main/MainPage.vue
@@ -2,19 +2,6 @@
   <main>
     <div class="flex flex-row justify-between">
       <div class="flex flex-col w-full">
-        <nav class="h-16 flex flex-row items-start justify-between">
-          <Select placeholder="Поиск рецептов" class="w-64" searchable></Select>
-          <AuthForm v-if="!isSignedIn" />
-          <div class="flex flex-row" v-else>
-            <ProfileMenu />
-            <BaseButton
-              class="mt-0 w-auto text-white font-bold bg-tomato-800 hover:bg-tomato-900 px-5 ml-5 py-2"
-              type="link"
-              to="/recipes/add"
-              text="Добавить рецепт"
-            />
-          </div>
-        </nav>
         <section class="mt-16">
           <!--          <h3 class="text-gray-600 text-2xl font-medium">Блок 1</h3>-->
           <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mt-6">
@@ -56,16 +43,10 @@
 </template>
 
 <script setup lang="ts">
-import Select from '@/components/base/Select.vue';
-import ProfileMenu from '@/components/profile/ProfileMenu.vue';
-import AuthForm from '@/components/forms/AuthForm.vue';
 import { computed, onMounted } from 'vue';
 import { useAuthStore } from '@/stores/auth';
-import BaseButton from '@/components/base/BaseButton.vue';
 
 const auth = useAuthStore();
-const isSignedIn = computed<boolean>(() => Object.values(auth.getMe).length > 0);
-
 onMounted(async () => {
   await auth.me();
 });
