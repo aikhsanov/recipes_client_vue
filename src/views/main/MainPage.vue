d7b214921c96d88e521fee4b203c2435fe4e410d
aikhsanov <adrstrings@gmail.com>
2023-09-18 13:30:21 +0300

    !wip recipes MainPage

--- src/views/main/MainPage.vue
+++ src/views/main/MainPage.vue
@@ -4,21 +4,41 @@
       <section class="mt-16">
         <!--          <h3 class="text-gray-600 text-2xl font-medium">Блок 1</h3>-->
         <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mt-6">
-          <div
-            class="templ col-span-2 h-80 w-full mx-auto rounded-md shadow-md overflow-hidden"
-          ></div>
-          <div
-            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"
-          ></div>
-          <div
-            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"
-          ></div>
-          <div
-            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"
-          ></div>
-          <div
-            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"
-          ></div>
+          <!--          <div-->
+          <!--            class="templ col-span-2 h-80 w-full mx-auto rounded-md shadow-md overflow-hidden"-->
+          <!--          ></div>-->
+          <!--          <div-->
+          <!--            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"-->
+          <!--          ></div>-->
+          <!--          <div-->
+          <!--            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"-->
+          <!--          ></div>-->
+          <!--          <div-->
+          <!--            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"-->
+          <!--          ></div>-->
+          <!--          <div-->
+          <!--            class="w-full h-80 max-w-sm mx-auto rounded-md shadow-md overflow-hidden templ"-->
+          <!--          ></div>-->
+          <router-link
+            v-for="(recipe, ind) in recipes.getRecipes"
+            :key="recipe.title"
+            :to="`/recipes/${recipe.id}`"
+            :class="`w-full h-80  mx-auto rounded-md shadow-md overflow-hidden relative ${
+              ind !== 0 ? 'max-w-sm' : 'col-span-2 '
+            }`"
+          >
+            <div class="w-full h-full">
+              <img :src="recipe.img_url" alt="" class="w-full h-full object-cover" />
+              <div
+                class="recipe-preview-text w-full absolute bottom-0 p-2 opacity-80 h-1/3 bg-white"
+              >
+                <h3 class="font-bold mb-2 line-clamp-2">{{ recipe.title }}</h3>
+                <p :class="`w-full ${recipe.title.length > 75 ? 'line-clamp-1' : 'line-clamp-2'}`">
+                  {{ recipe.short_dsc }}
+                </p>
+              </div>
+            </div>
+          </router-link>
         </div>
       </section>
       <section class="mt-16">
