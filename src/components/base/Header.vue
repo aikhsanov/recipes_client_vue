6cd9006b6caaa504116870a3f987bb30875fe5d3
aikhsanov <adrstrings@gmail.com>
2023-10-10 12:52:24 +0300

    !wip searchResults infinite scroll

---
+++ src/components/base/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="p-4 border-2 border-b-gray-200">
+    <nav class="h-11 flex flex-row items-start container justify-between container mx-auto">
+      <router-link to="/" class="w-3/12">
+        <h1 class="text-4xl text-gray-500 font-bold">Едим Вкусно</h1>
+      </router-link>
+
+      <AuthForm v-if="!auth.getIsAuthed" />
+      <div class="flex flex-row" v-else>
+        <ProfileMenu />
+        <BaseButton
+          class="mt-0 w-auto text-white font-bold bg-tomato-800 hover:bg-tomato-900 px-5 ml-5 py-2"
+          type="link"
+          to="/recipes/add"
+          text="Добавить рецепт"
+        />
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,22 +1,4 @@
 <template>
-  <header class="p-4 border-2 border-b-gray-200">
-    <nav class="h-11 flex flex-row items-start container justify-between container mx-auto">
-      <router-link to="/" class="w-3/12">
-        <h1 class="text-4xl text-gray-500 font-bold">Едим Вкусно</h1>
-      </router-link>
-
-      <AuthForm v-if="!auth.getIsAuthed" />
-      <div class="flex flex-row" v-else>
-        <ProfileMenu />
-        <BaseButton
-          class="mt-0 w-auto text-white font-bold bg-tomato-800 hover:bg-tomato-900 px-5 ml-5 py-2"
-          type="link"
-          to="/recipes/add"
-          text="Добавить рецепт"
-        />
-      </div>
-    </nav>
-  </header>
   <div class="container mx-auto min-h-screen py-4">
     <main>
       <div class="flex flex-row justify-between">
