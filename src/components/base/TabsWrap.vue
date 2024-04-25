45a6b046aafcb76524a4c95593b03d798e7a6c4a
ai-strings <adrstrings@gmail.com>
2024-04-25 21:44:24 +0300

    +add tabs styles

--- src/components/base/TabsWrap.vue
+++ src/components/base/TabsWrap.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="">
-    <div class="flex flex-row">
-      <h2 v-for="(tab, ind) in slotsChildren" @click="() => changeIndex(ind)">
+    <div class="flex flex-row justify-start items-center my-5 border-b-2 px-5 py-1">
+      <span
+        :key="ind"
+        v-for="(tab, ind) in slotsChildren"
+        @click="() => changeIndex(ind)"
+        :class="`mr-5 text-md hover:cursor-pointer tab-title ${tabIndex === ind ? 'active' : ''}`"
+      >
         {{ tab?.props?.title }}
-      </h2>
+      </span>
     </div>
     <component :is="slotsChildren?.[tabIndex]" :key="tabIndex" />
   </div>
@@ -18,4 +23,24 @@ const { tabIndex, changeIndex } = useTabs();
 console.log(slotsChildren);
 </script>
 
-<style scoped></style>
+<style scoped>
+.tab-title {
+  position: relative;
+}
+.tab-title::after {
+  content: '';
+  position: absolute;
+  height: 2px;
+  bottom: -5px;
+  left: 0;
+  width: 100%;
+  background: #357ba1;
+  opacity: 0;
+}
+.tab-title:hover::after {
+  opacity: 1;
+}
+.tab-title.active:after {
+  opacity: 1;
+}
+</style>
