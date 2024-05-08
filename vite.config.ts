5460ee7d1217cb4e2dbe9cb4177c42b826ed7064
ai-strings <adrstrings@gmail.com>
2024-05-08 11:38:18 +0300

    !fix body overflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dynamicImport(/* options */)],
+  build: {
+    outDir: 'build',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
